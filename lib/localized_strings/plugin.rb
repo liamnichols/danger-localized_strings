6e1e1d57c9a10aa15057b67172b11994103738c5
Liam Nichols <liam-nichols@cookpad.jp>
2018-02-22 18:05:02 +0000

    Removed comments and tests

--- lib/localized_strings/plugin.rb
+++ lib/localized_strings/plugin.rb
@@ -3,22 +3,7 @@
 require "json"
 
 module Danger
-  # This is your plugin class. Any attributes or methods you expose here will
-  # be available from within your Dangerfile.
   #
-  # To be published on the Danger plugins site, you will need to have
-  # the public interface documented. Danger uses [YARD](http://yardoc.org/)
-  # for generating documentation from your plugin source, and you can verify
-  # by running `danger plugins lint` or `bundle exec rake spec`.
-  #
-  # You should replace these comments with a public description of your library.
-  #
-  # @example Ensure people are well warned about merging on Mondays
-  #
-  #          my_plugin.warn_on_mondays
-  #
-  # @see  Liam Nichols/danger-localized_strings
-  # @tags monday, weekends, time, rattata
   #
   class DangerLocalizedStrings < Plugin
     # Verify
--- spec/localized_strings_spec.rb
+++ spec/localized_strings_spec.rb
@@ -6,36 +6,14 @@ module Danger
       expect(Danger::DangerLocalizedStrings.new(nil)).to be_a Danger::Plugin
     end
 
-    # #
-    # # You should test your custom attributes and methods here
-    # #
-    # describe "with Dangerfile" do
-    #   before do
-    #     @dangerfile = testing_dangerfile
-    #     @my_plugin = @dangerfile.localized_strings
-    #   end
-
-    #   # Some examples for writing tests
-    #   # You should replace these with your own.
-
-    #   it "Warns on a monday" do
-    #     monday_date = Date.parse("2016-07-11")
-    #     allow(Date).to receive(:today).and_return monday_date
-
-    #     @my_plugin.warn_on_mondays
-
-    #     expect(@dangerfile.status_report[:warnings]).to eq(["Trying to merge code on a Monday"])
-    #   end
-
-    #   it "Does nothing on a tuesday" do
-    #     monday_date = Date.parse("2016-07-12")
-    #     allow(Date).to receive(:today).and_return monday_date
-
-    #     @my_plugin.warn_on_mondays
-
-    #     expect(@dangerfile.status_report[:warnings]).to eq([])
-    #   end
-
-    # end
+    #
+    # Test the plugin within a Dangerfile
+    #
+    describe "with Dangerfile" do
+      before do
+        @dangerfile = testing_dangerfile
+        @localized_strings = @dangerfile.localized_strings
+      end
+    end
   end
 end
